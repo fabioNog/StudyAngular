d2371425e7d8a6ee6f190c14076487988455e1cc
fabioNog <fabio.carmo@computacao.ufla.br>
2019-12-25 21:52:54 -0300

    Creando cursosDetalhes

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
+import { CursosDetalhesComponent } from './cursos-detalhes/cursos-detalhes.component';
 
 
 
 @NgModule({
-  declarations: [CursosComponent],
+  declarations: [
+    CursosComponent, 
+    CursosDetalhesComponent
+  ],
   imports: [
     CommonModule
   ],
